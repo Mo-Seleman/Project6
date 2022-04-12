0678c0b82f58936a5f048e4d4923df24a07c00ed
Mo-Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2022-04-12 15:14:09 +0100

    checkWin
    Working on CheckWin Function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const btnReset = document.querySelector('.btn__reset');
 const keyboardBtn = document.querySelector('button');
 const chosen = document.querySelector('.chosen');
 const hearts = document.querySelectorAll("img");
+const overlay = document.querySelector('#overlay');
 
 let missed = 0;
  
@@ -44,30 +45,43 @@ function addPhraseToDisplay(arr) {
 
 addPhraseToDisplay(output);
 
+/*Check Letter Function*/
 function checkLetter (buttonClicked) {
   const liLetter = document.querySelectorAll('.letter');
   let match = null;
   for (let i = 0; i < liLetter.length; i++) {
-    if(liLetter[i].textContent.toLowerCase() === buttonClicked.textContent){
-      liLetter[i].classList.add("show");
+    if(liLetter[i].textContent.toLowerCase() === buttonClicked.textContent.toLowerCase()){
+      liLetter[i].classList.add("show"); /*Where Show Is Added*/
       match = buttonClicked.textContent;
     }
-  } return match;
+  } return match
 };
 
+/*Keyboard Event Listener*/
 const click  = document.addEventListener('click', (e) => {
   const target = e.target; 
   if (target.tagName === 'BUTTON') {
     target.className = 'chosen';
     target.setAttribute("disabled", "");
-    const letterFound = checkLetter(target);
+    const letterFound = checkLetter(target); /*Check Letter Function*/
     if (letterFound === null) {
-    missed ++;
     const hearts = document.querySelectorAll("img");
-    for (let i = 0; i < hearts.length; i++)
-    var loopHearts = hearts[i];
-    loopHearts[2].setAttribute("src", "images/lostHeart.png");  
-  } 
- } 
-});
+    hearts[missed].setAttribute("src", "images/lostHeart.png");  
+    missed ++;
+  }
+ } /* checkWin(); */
+})
+
+const liLetter = document.querySelectorAll('.letter');
+const show = document.querySelectorAll('.show');
+
+console.log(liLetter.length); /*Getting Length of liLetter*/
+console.log(show.length)/*Not Working-Consider How It Wont Register Until Triggered*/
+
+/*Checking For Win*/
+function checkWin(){
+  if (liLetter.length === show.length) {
+    overlay.classList.add("win");
+  }
+};
 
